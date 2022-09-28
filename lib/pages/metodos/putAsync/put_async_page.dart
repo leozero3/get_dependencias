0e06cd1604fe8c90cacad1bb4fe08f6803eaa5af
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2022-09-28 17:46:43 -0300

    putAsync - 3:39

--- lib/pages/metodos/putAsync/put_async_page.dart
+++ lib/pages/metodos/putAsync/put_async_page.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PutAsyncPage extends StatelessWidget {
-  const PutAsyncPage({Key? key}) : super(key: key);
+   PutAsyncPage({Key? key}) : super(key: key){
+    Get.putAsync(() async{
+
+      final prefs = SharedPreferences.getInstance()
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
